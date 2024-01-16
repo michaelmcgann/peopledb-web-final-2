7839e1a8b111d14621cb5a062cf2cecc672b234f
Mike <126602126+michaelmcgann@users.noreply.github.com>
2024-01-16 17:01:20 +0000

    Initial commit

---
+++ src/test/java/com/michael/peopledbweb/PeopledbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.peopledbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopledbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
